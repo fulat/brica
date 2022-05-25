fbe497d0c4e054bb40f400117a01519ff0ad5a3e
Brayhan De Aza <brayhandeaza@gmail.com>
2022-05-24 21:01:45 -0400

    imageModal component cleaned and migrate to function component

--- front-end/src/components/post/ImageModals.jsx
+++ front-end/src/components/post/ImageModals.jsx
@@ -1,55 +1,41 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import { Modal } from 'antd'
 import { faCircleXmark } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-class ImageModals extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
+
+function Modals(props) {
+    const { posts } = props.state
+
+    const handleOk = () => {
+        props.dispatch({ type: "HIDE_IMAGE_MODAL" })
     }
 
-    handleOk = () => {
-        this.props.dispatch({ type: "HIDE_IMAGE_MODAL" })
+    const handleCancel = () => {
+        props.dispatch({ type: "HIDE_IMAGE_MODAL" })
     }
 
-    handleCancel = () => {
-        this.props.dispatch({ type: "HIDE_IMAGE_MODAL" })
-    }
+    const ModalIcon = () => (
+        <div className="emoji-container-close-icon mt-2 ms-2">
+            <FontAwesomeIcon className='hover emoji-close-icon ps-3' icon={faCircleXmark} />
+        </div>
+    )
 
-    render() {
-        const { posts } = this.props.state
-        return (
-            <div className="ImageModals">
-                <Modal
-                    width={"50%"}
-                    style={{ borderRadius: 15 }}
-                    closeIcon={
-                        <div className="emoji-container-close-icon mt-2 ms-2">
-                            <FontAwesomeIcon
-                                className='hover emoji-close-icon ps-3'
-                                icon={faCircleXmark} />
-                        </div>
-                    }
-                    footer={false}
-                    centered
-                    title=""
-                    visible={posts.showImageModal}
-                    onOk={this.handleOk}
-                    onCancel={this.handleCancel}
-
-                >
-                    <div className='col-12' style={{ display: "flex", flexDirection: "column", width: "100%"}}>
-                        <img style={{borderRadius: 10}} src={posts.showImageModalData} alt="" srcSet="" />
-                    </div >
-                </Modal>
-            </div>
-        );
-    }
+    return (
+        <div className="ImageModals">
+            <Modal width={"50%"} style={{ borderRadius: 15 }} footer={false} centered title="" visible={posts.showImageModal} onOk={handleOk} onCancel={handleCancel} closeIcon={ModalIcon}>
+                <div className='col-12' style={{ display: "flex", flexDirection: "column", width: "100%" }}>
+                    <img style={{ borderRadius: 10 }} src={posts.showImageModalData} alt="" srcSet="" />
+                </div>
+            </Modal>
+        </div>
+    )
 }
+
+
 const mapStateToProps = (state, ownProps) => ({
     state, ownProps
 })
 
-export default connect(mapStateToProps)(ImageModals);
+export default connect(mapStateToProps)(Modals)
